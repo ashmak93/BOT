322f3f98a65338717e0c192fd5cf78a01082a39d
Aisha Shalbuzova <aisharizvanovna@mail.ru>
2025-12-14 21:11:49 +0300

    тест оср

--- handlers/photo_handlers.py
+++ handlers/photo_handlers.py
@@ -3,17 +3,19 @@ from aiogram.filters import Command
 from services.ocr_service import OCRService
 import logging
 import io
+from aiogram import Bot
 
 router = Router()
 logger = logging.getLogger(__name__)
 
-@router.message(lambda message: message.photo is not None)
-async def handle_image(message: types.Message):
+
+@router.message(lambda message: message.photo)
+async def handle_image(message: types.Message, bot: Bot):
     """Обрабатывает изображение и отправляет распознанный текст"""
     try:
         photo = message.photo[-1]
 
-        photo_file = await photo.download(destination=io.BytesIO())
+        photo_file = await bot.download(photo.file_id, destination=io.BytesIO())
         image_bytes = photo_file.getvalue()
 
         text = OCRService.extract_text_from_image(image_bytes)
