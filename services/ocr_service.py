4513d2f415e1702db2c815843d36c800b3cffe09
Aisha Shalbuzova <aisharizvanovna@mail.ru>
2025-12-14 21:14:27 +0300

    тест оср

--- services/ocr_service.py
+++ services/ocr_service.py
@@ -2,6 +2,8 @@ import pytesseract
 from PIL import Image
 import io
 
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
 class OCRService:
     @staticmethod
     def extract_text_from_image(image_bytes):
