7563abec10d7a9248f2b357b75a78466b0213655
Aisha Shalbuzova <aisharizvanovna@mail.ru>
2025-12-14 19:40:27 +0300

    тест оср

---
+++ services/ocr_service.py
@@ -0,0 +1,14 @@
+import pytesseract
+from PIL import Image
+import io
+
+class OCRService:
+    @staticmethod
+    def extract_text_from_image(image_bytes):
+        """Извлекает текст из изображения"""
+        try:
+            image = Image.open(io.BytesIO(image_bytes))
+            text = pytesseract.image_to_string(image, lang='rus+eng')
+            return text.strip() if text else "Текст не найден"
+        except Exception as e:
+            return f"Ошибка обработки: {str(e)}"
